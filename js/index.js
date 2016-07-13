c315fd8a6bc4608d25be481e48324d811a9d0e42
renoumu <renoumu@126.com>
2016-07-13 20:00:38 +0800

    ‘del’

---
+++ js/index.js
@@ -0,0 +1,19 @@
+// JavaScript Document
+function id(obj) {
+    return document.getElementById(obj);
+}
+function bind(obj, ev, fn) { 
+    if (obj.addEventListener) {
+        obj.addEventListener(ev, fn, false);
+    } else {
+        obj.attachEvent('on' + ev, function() {
+            fn.call(obj);
+        });
+    }
+}
+function view() {
+    return {
+        w: document.documentElement.clientWidth,
+        h: document.documentElement.clientHeight
+    };
+}
